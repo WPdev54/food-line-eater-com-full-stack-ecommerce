1ebaae651610bfbdc2b47b8455b762713276cecc
WPDEV <wpdevolper54@gmail.com>
2023-10-21 23:58:51 +0600

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.port || 5000;
+// const ObjectId = require('mongodb').ObjectID
+
+app.use(cors());
+app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process?.env?.DB_USER}:${process.env.DB_PASS}@cluster0.xrv2f5n.mongodb.net/?retryWrites=true&w=majority`;
@@ -19,6 +23,7 @@ async function run() {
   try {
     await client.connect();
 
+    const usersCollection = client.db("eaterDB").collection("user")
     const menuCollection = client.db("eaterDB").collection("menu")
     const cartCollection = client.db("eaterDB").collection("carts")
 
@@ -47,6 +52,19 @@ async function run() {
       const result = await cartCollection.insertOne(item)
       res.send(result)
     })
+    app.post('/user', async (req, res) => {
+      const user = req.body
+      // console.log(item);
+      const result = await usersCollection.insertOne(user)
+      res.send(result)
+    })
+
+    app.delete('/carts/:id', async (req, res) => {
+      const _id = req.params.id;
+      const query = { _id: new ObjectID(_id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
 
@@ -66,8 +84,6 @@ run().catch(console.dir);
 *Password: XuFWph5eQYgoom51
  */
 
-app.use(cors());
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send(`Boss is Coming From Port ${port}`);
